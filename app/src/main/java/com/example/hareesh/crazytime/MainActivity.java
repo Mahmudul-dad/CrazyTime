70f6d09101d85f9af289dc082d25508f3f85a056
Kruthika Tanikella <kruhikat@yahoo.com>
2016-05-20 00:34:38 -0700

    Fixed aws library issue and cleaned code

--- app/src/main/java/com/example/hareesh/crazytime/MainActivity.java
+++ app/src/main/java/com/example/hareesh/crazytime/MainActivity.java
@@ -10,25 +10,23 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.GestureDetector;
 import android.view.Gravity;
-import android.view.MotionEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.VelocityTracker;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
-import android.widget.ScrollView;
+import android.widget.Scroller;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.view.ViewTreeObserver;
-import android.view.VelocityTracker;
 
 import junit.framework.Assert;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Random;
-
 
 public class MainActivity extends AppCompatActivity   {
 
@@ -38,7 +36,6 @@ public class MainActivity extends AppCompatActivity   {
      */
 
     private GestureDetectorCompat mDetector;
-
 
     //running event repeatedly
     private int mInterval = 10000; // 10 seconds by default, can be changed later
@@ -64,7 +61,6 @@ public class MainActivity extends AppCompatActivity   {
         String formattedDate = df.format(c.getTime());
         // formattedDate have current date/time
         Toast.makeText(this, formattedDate, Toast.LENGTH_SHORT).show();
-
 
         // Now we display formattedDate value in TextView
         TextView texttime = (TextView) findViewById(R.id.textTime);
@@ -94,7 +90,6 @@ public class MainActivity extends AppCompatActivity   {
         }
     }
 
-
     private View getImageView(Integer image) {
         ImageView imageView = new ImageView(getApplicationContext());
         LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
@@ -104,38 +99,35 @@ public class MainActivity extends AppCompatActivity   {
         return imageView;
     }
 
-
-
     Runnable mStatusChecker = new Runnable() {
         @Override
         public void run() {
             try {
-                Log.i("10", "interval");
-//                RelativeLayout linearLayout = (RelativeLayout) findViewById(R.id.relativeLayoutid);
-//
-//                Calendar c = Calendar.getInstance();
-//                long now = c.getTimeInMillis();
-//                c.set(Calendar.HOUR_OF_DAY, 0);
-//                c.set(Calendar.MINUTE, 0);
-//                c.set(Calendar.SECOND, 0);
-//                c.set(Calendar.MILLISECOND, 0);
-//                long passed = now - c.getTimeInMillis();
-//                long secondsPassed = passed / 1000;
-//
-//                Log.i("image number", "" + secondsPassed);
-//
-//                Random random = new Random();
-//                int r = random.nextInt(5);
-//
-//                String uri = "@drawable/sequence000" + r;  // where myresource (without the extension) is the file
-//
-//                int imageResource = getResources().getIdentifier(uri, null, getPackageName());
-//
-////                imageview= (ImageView)findViewById(R.id.imageView);
-////                Drawable resImage = getResources().getDrawable(imageResource);
-////                imageView.setImageDrawable(res);
-//
-//                linearLayout.setBackgroundResource(imageResource);
+                RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayoutid);
+
+                Calendar c = Calendar.getInstance();
+                long now = c.getTimeInMillis();
+                c.set(Calendar.HOUR_OF_DAY, 0);
+                c.set(Calendar.MINUTE, 0);
+                c.set(Calendar.SECOND, 0);
+                c.set(Calendar.MILLISECOND, 0);
+                long passed = now - c.getTimeInMillis();
+                long secondsPassed = passed / 1000;
+
+                Log.i("image number", "" + secondsPassed);
+
+                Random random = new Random();
+                int r = random.nextInt(5);
+
+                String uri = "@drawable/sequence000" + r;  // where myresource (without the extension) is the file
+
+                int imageResource = getResources().getIdentifier(uri, null, getPackageName());
+
+//                imageview= (ImageView)findViewById(R.id.imageView);
+//                Drawable resImage = getResources().getDrawable(imageResource);
+//                imageView.setImageDrawable(res);
+
+                relativeLayout.setBackgroundResource(imageResource);
 
                  //this function can change value of mInterval.
             } finally {
@@ -162,12 +154,6 @@ public class MainActivity extends AppCompatActivity   {
     void stopRepeatingTask() {
         mHandler.removeCallbacks(mStatusChecker);
     }
-
-//    @Override
-//    public boolean onTouchEvent(MotionEvent event){
-//
-//    }
-
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
@@ -203,6 +189,10 @@ public class MainActivity extends AppCompatActivity   {
                 Log.d("", "X velocity: " +
                         VelocityTrackerCompat.getXVelocity(mVelocityTracker,
                                 pointerId));
+                float Xvel = VelocityTrackerCompat.getXVelocity(mVelocityTracker,pointerId);
+                float YVel = VelocityTrackerCompat.getYVelocity(mVelocityTracker,pointerId);
+
+
                 Log.d("", "Y velocity: " +
                         VelocityTrackerCompat.getYVelocity(mVelocityTracker,
                                 pointerId));
@@ -235,15 +225,26 @@ public class MainActivity extends AppCompatActivity   {
 
         @Override
         public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+            Scroller mScroller = new Scroller(getApplicationContext());
 
             Log.i(DEBUG_TAG, "Scrolling wooohoooooo");
+
+            if (mScroller.computeScrollOffset()) {
+                // Get current x and y positions
+                int currX = mScroller.getCurrX();
+                int currY = mScroller.getCurrY();
+
+                Log.i("x pos", "" + currX);
+                Log.i("y pos", "" + currY);
+            } else {
+                Log.i("no pos", "no pos");
+            }
 
             mHandler.postDelayed(new Runnable() {
 
                 @Override
                 public void run() {
                     try {
-                        Log.i("10", "interval");
                         RelativeLayout linearLayout = (RelativeLayout) findViewById(R.id.relativeLayoutid);
 
                         Calendar c = Calendar.getInstance();
@@ -264,67 +265,19 @@ public class MainActivity extends AppCompatActivity   {
 
                         int imageResource = getResources().getIdentifier(uri, null, getPackageName());
 
-                        ScrollView scrollView = (ScrollView) findViewById(R.id.scrollView);
-                        Log.i("scrollPos", "" + scrollView.getScrollY());
-
-//                imageview= (ImageView)findViewById(R.id.imageView);
-//                Drawable resImage = getResources().getDrawable(imageResource);
-//                imageView.setImageDrawable(res);
-
                         linearLayout.setBackgroundResource(imageResource);
-
-                        //this function can change value of mInterval.
                     }
                     finally {
                         // 100% guarantee that this always happens, even if
                         // your update method throws an exception
                         mHandler.postDelayed(mStatusChecker, 0);
                     }
-                    //wait = false;
                 }
             }, 0);
-
-
-//            float deltaY = e2.getY() - e1.getY();
-//            float deltaX = e2.getX() - e1.getX();
-//
-//            if (Math.abs(deltaX) > Math.abs(deltaY)) {
-//                if (Math.abs(deltaX) > SWIPE_THRESHOLD) {
-//                    listener.onHorizontalScroll(e2, deltaX);
-//                    if (deltaX > 0) {
-//                        Log.i("tag", "Slide right");
-//                    } else {
-//                        Log.i("tag", "Slide left");
-//                    }
-//                }
-//            } else {
-//                if (Math.abs(deltaY) > 100) {
-//                    listener.onVerticalScroll(e2, deltaY);
-//                    if (deltaY > 0) {
-//                        Log.i(TAG, "Slide down");
-//                    } else {
-//                        Log.i(TAG, "Slide up");
-//                    }
-//                }
-//            }
             return true;
         }
 
     }
-
-//    @Override
-//    public void onScrollChanged(){
-//
-//        Log.i("scrollPos", "");
-//        ScrollView scrollView = (ScrollView) findViewById(R.id.scrollView);
-//
-//        int scrollX = scrollView.getScrollX(); //for horizontalScrollView
-//        int scrollY = scrollView.getScrollY(); //for verticalScrollView
-//
-//        Log.i("scrollPos", "" + scrollY);
-//        //DO SOMETHING WITH THE SCROLL COORDINATES
-//
-//    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -347,24 +300,4 @@ public class MainActivity extends AppCompatActivity   {
 
         return super.onOptionsItemSelected(item);
     }
-
-
-//    @Override
-//    public void surfaceChanged(SurfaceHolder holder, int format, int width,
-//                               int height) {
-//        // TODO Auto-generated method stub
-//
-//    }
-//
-//    @Override
-//    public void surfaceCreated(SurfaceHolder holder) {
-//        // TODO Auto-generated method stub
-//
-//    }
-//
-//    @Override
-//    public void surfaceDestroyed(SurfaceHolder holder) {
-//        // TODO Auto-generated method stub
-//
-//    }
 }
